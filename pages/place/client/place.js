3b7d303edf584f58d38ebe79e042509b8daf4e21
fatima1996 <fatimahussaini@fatimas-MacBook-Air.local>
2016-06-27 19:25:37 -0400

    pa1

--- pages/place/client/place.js
+++ pages/place/client/place.js
@@ -17,6 +17,24 @@ Template.place.events({
 		const trip =
 		{location:dest, date_entered:enter, date_leaving:bye, budget:[low, med, high]};
 		console.dir(trip);
-		Places.insert(trip);
-	}
+		//Places.insert(trip);
+		Meteor.call("insertPlace",trip)
+	},
+	"click .js-removetrip" : function(event) {
+		Meteor.call("removeTrip",this._id);
+	},
+	"click .js-removeAll" : function(event) {
+		const pw = $(".js-password").val();
+		Meteor.call("removeAll", pw)
+	},
 })
+function drawTiltedRectangle() {
+	drawContext = drawSpace.getContext("2d");
+	drawContext.strokeStyle="#00ff00"; // or green or "#fff"
+	drawContext.moveTo(100,100);
+	drawContext.lineTo(150,100);
+	drawContext.lineTo(125,200);
+	drawContext.lineTo(75,200);
+	drawContext.lineTo(100,100)
+	drawContext.stroke();
+}
